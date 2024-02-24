59a94e29db3491c1b3782e497c27ba08c615874b
Saksham Singh <mailtosakshamsingh@gmail.com>
2024-02-24 00:41:17 -0800

    added functioning top navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'community-connect';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { FeedComponent } from './feed/feed.component';
+import { LandingComponent } from './landing/landing.component';
+import { RecommendationsComponent } from './recommendations/recommendations.component';
+import { SkillsComponent } from './skills/skills.component';
+
+export const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'feed', component: FeedComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'recommendations', component: RecommendationsComponent },
+
+  // {
+  //   path: '',
+  //   loadComponent: () =>
+  //     import('./landing/landing.component').then((mod) => mod.LandingComponent),
+  // },
+  // {
+  //   path: 'feed',
+  //   loadComponent: () =>
+  //     import('./feed/feed.component').then((mod) => mod.FeedComponent),
+  // },
+  // {
+  //   path: 'skills',
+  //   loadComponent: () =>
+  //     import('./skills/skills.component').then((mod) => mod.SkillsComponent),
+  // },
+  // {
+  //   path: 'recommendations',
+  //   loadComponent: () =>
+  //     import('./recommendations/recommendations.component').then(
+  //       (mod) => mod.RecommendationsComponent
+  //     ),
+  // },
+];
